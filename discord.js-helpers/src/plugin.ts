953f597e58765bf7e8364af7612ef8942b571a4d
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-06-08 17:40:39 +0100

    Update plugin.ts

--- discord.js-helpers/src/plugin.ts
+++ discord.js-helpers/src/plugin.ts
@@ -1,14 +1,18 @@
 import { Client, SlashCommandBuilder } from "discord.js";
 
+import type { BotConfig, ResolvedBotConfig } from "./bot.js";
 import type { CommandModule } from "./commands.js";
 import type { EventListenerModule } from "./event-listeners.js";
 
-export type Plugin = {
+type PluginConfig = {
 	name: string;
 	commands?: Record<string, CommandModule<SlashCommandBuilder>>;
 	eventListeners?: Record<string, EventListenerModule<any>>;
+	resolvedConfig?: (resolvedConfig: ResolvedBotConfig) => void;
 	onReady?: (client: Client<true>) => void | Promise<void>;
 };
+
+export type Plugin = PluginConfig | ((config: BotConfig) => PluginConfig);
 
 export function definePlugin(config: Plugin) {
 	return config;
