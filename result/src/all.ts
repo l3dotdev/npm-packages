fc8e7bd151bc3b2d09d975f060e726a7ca7dd361
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-12-28 11:55:38 +0000

    Update all.ts

--- result/src/all.ts
+++ result/src/all.ts
@@ -23,25 +23,7 @@ export async function allAsync<
 	| Extract<Awaited<TResults[number]>, Err<any, any>>
 	| Ok<Extract<Awaited<TResults[number]>, Ok<any>>["value"][]>
 > {
-	const values = [];
-	for (const asyncResult of results) {
-		const result = await asyncResult;
-		if (!result.ok) {
-			return result as Extract<Awaited<TResults[number]>, Err<any, any>>;
-		}
-		values.push(result.value);
-	}
-	return ok(values);
-}
-
-export async function allSettledAsync<
-	TResults extends (ReturnResult<any, any> | Promise<ReturnResult<any, any>>)[]
->(...results: TResults): Promise<Awaited<TResults[number]>[]> {
-	const settled: Awaited<TResults[number]>[] = [];
-	for (const asyncResult of results) {
-		settled.push((await asyncResult) as Awaited<TResults[number]>);
-	}
-	return settled;
+	return all(...(await Promise.all(results)));
 }
 
 export function allValues<TResults extends ReturnResult<any, any>[]>(
